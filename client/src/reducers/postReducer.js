e19d0b934612113c3bc7f1ea3c4b9d85fc247d15
prashant0664 <prashantthakran0664@gmail.com>
2024-05-31 14:40:53 +0530

    added readme.md

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -4,7 +4,7 @@ let posts = [];
 export function postReducer(state = posts, action) {
   switch (action.type) {
     case "SET_POSTS":
-      return [...state, ...action.payload];
+      return [...state, action.payload];
     default:
       return state;
   }
