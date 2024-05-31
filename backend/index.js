379225b77c6cc6357463700964e835feefc4485e
prashant0664 <prashantthakran0664@gmail.com>
2024-05-31 22:07:15 +0530

    added readme.md

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ var store = new MongoDBStore(
 );
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "Backend url"); 
+  res.setHeader('Access-Control-Allow-Origin', "https://all-blogs-v2.onrender.com"); 
   res.setHeader('Access-Control-Allow-Credentials', 'true'); 
   next();
 });
